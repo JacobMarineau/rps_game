b0692c39e0292f9fcfdf2846016ef9a1a39c74ed
JacobMarineau <jacobmarineaump@gmail.com>
2024-05-17 15:34:10 -0500

    First attempt

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hello you");
+
+let rock = "rock";
+let paper = "paper";
+let scissor = "scissor";
+
+function getComputerChoice(rock, paper, scissor) {
+  return Math.floor(Math.random(rock, paper, scissor));
+}
